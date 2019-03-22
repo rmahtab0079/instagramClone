8a0cc19b20bc9930e0d723bc428c4050e222e2f8
Rageeb Mahtab <>
2019-03-21 21:39:02 -0400

    First build of the fully functional instagram app

---
+++ Parstagram/PostTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram
+//
+//  Created by Rageeb Mahtab on 3/21/19.
+//  Copyright © 2019 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
